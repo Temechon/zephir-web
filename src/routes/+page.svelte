4069f08c44b30e216b1032a7d6de0c4d30fd0c10
Julian <julian.chenard@gmail.com>
2023-01-19 10:50:40 +0100

    Tailwind added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <title>Zephir | A project manager software</title>
+</svelte:head>
+
+Bonjouuuur
+<div class="bg-red-200">coucou</div>
